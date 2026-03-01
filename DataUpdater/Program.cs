eec103af725dbeafc4615f52b8ea6c295eb9c3ed
Dan Carter <djcarter85@gmail.com>
2026-03-01 21:17:48 +0000

    feat: write file locally

--- DataUpdater/Program.cs
+++ DataUpdater/Program.cs
@@ -10,4 +10,4 @@ var pointsTable = PointsTableGenerator.Generate(table);
 
 var json = JsonSerializer.Serialize(pointsTable, options: new() { WriteIndented = true });
 
-Console.WriteLine(json);
+await File.WriteAllTextAsync(@"..\..\..\..\src\data.json", json);
