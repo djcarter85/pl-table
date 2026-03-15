1b4f1d4e1b8c7eab4304ab4ab9222791da394f53
Dan Carter <djcarter85@gmail.com>
2026-03-15 20:08:56 +0000

    test: add simple unit test for points table generation

---
+++ DataUpdater/DataUpdater.UnitTests/PointsTableGeneratorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace DataUpdater.UnitTests;
+
+using NUnit.Framework;
+
+public static class PointsTableGeneratorTests
+{
+    [Test]
+    public static void Table_with_single_team_results_in_single_entry()
+    {
+        IReadOnlyList<TableEntry> table = [new(Position: 1, TeamShortName: "ARS", Played: 10, Points: 25)];
+        var pointsTable = PointsTableGenerator.Generate(table);
+        
+        Assert.That(pointsTable.Entries.Count(), Is.EqualTo(1));
+    }
+}
